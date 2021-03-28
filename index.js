790afd21556e6b968db17f6b1bcca9af8667b6d5
ward438 <75048548+ward438@users.noreply.github.com>
2021-03-28 17:51:17 -0500

    added cases, creating licensing

--- index.js
+++ index.js
@@ -7,34 +7,80 @@ const readline = require('readline').createInterface({
 const colors = require('colors');
 
 function createReadMe() {
-    readline.question('Header subject of ReadMe: '.red, title => {
-        console.log(`The Header input: ${title}`, '\n');
-        fs.appendFile('ReadMe.md', `${title}` + "\r\n" + "\r\n", (err) => {
-            if (err)
-                throw err;
-            readline.question('Description: '.red, description => {
-                console.log(`The subject description is: ${description}`, '\n');
-                fs.appendFile('ReadMe.md', `${description}` + "\r\n" + "\r\n", (err) => {
-                    if (err)
-                        throw err;
-                })
-                readline.question('Enter installation instructions: '.red, instructions => {
-                    console.log(`Installation instructions: ${instructions}`, '\n');
-                    fs.appendFile('ReadMe.md', `${instructions}` + "\r\n" + "\r\n", (err) => {
-                        if (err)
-                            throw err;
-                    })
-                    readline.close();
-                })
+    const hash = "# ";
+    let question = readline.question('README header: '.red, title => {
+        console.log(`Header is: ${title}`, '\n');
+        fs.appendFile('ReadMe.md', `${hash}` +
+            `${title}` + "\r\n" + "\r\n", (err) => {
+                if (err)
+                    throw err;
+                description();
 
             })
 
-        });
+    })
 
+    let description = function description() {
+        readline.question('Enter description of application or code: '.red, description => {
+            console.log(`Subject description: ${description}`, '\n');
+            fs.appendFile('ReadMe.md', `${description}` + "\r\n" + "\r\n", (err) => {
+                if (err)
+                    throw err;
+                instructions();
+            })
+        })
+    };
 
-    });
+    let instructions = function instructions() {
+        readline.question('Enter installation instructions: '.red, instructions => {
+            console.log(`Installation instructions: ${instructions}`, '\n');
+            fs.appendFile('ReadMe.md', `${instructions}` + "\r\n" + "\r\n", (err) => {
+                if (err)
+                    throw err;
+                usage();
+            })
+        })
+    };
+
+    let usage = function usage() {
+        readline.question('0 = Limited Usage: '.red + "\r\n" + '1 = Restricted Usage: '.green + "\r\n" + '2 = Unrestricted Usage: '.blue, usage => {
+            let elem;
+            switch (usage) {
+                case '0':
+                    elem = fs.appendFile('ReadMe.md', `test` + "\r\n" + "\r\n", (err) => {
+                        if (err)
+                            throw err;
+                        closing();
+
+                    })
+                    break;
+
+                case '1':
+                    elem = fs.appendFile('ReadMe.md', `${usage}` + "\r\n" + "\r\n", (err) => {
+                        if (err)
+                            throw err;
+                        closing();
+                    })
+                    break;
+
+                case '2':
+                    elem = fs.appendFile('ReadMe.md', `${usage}` + "\r\n" + "\r\n", (err) => {
+                        if (err)
+                            throw err;
+                        closing();
+                    })
+                    break;
+                default:
+                    console.log("Thank you")
+            }
+        })
+    };
+
+    let closing = function closing() {
+        readline.question('ReadMe complete!'.yellow, closing => {
+            console.log('README Complete!');
+        })
+        readline.close();
+    }
 };
-
-
-
 createReadMe();
